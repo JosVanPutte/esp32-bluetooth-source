9a27695aa66394bcb6b67d799cfbe416a4d22909
JosVanPutte <123905477+JosVanPutte@users.noreply.github.com>
2025-12-31 17:33:42 +0100

    changed to WAV format

--- AudioOutputBT.h
+++ AudioOutputBT.h
@@ -3,44 +3,44 @@
 #include "AudioOutput.h"
 #include "a2dp_source.h"
 
+static File inputfile;
+static uint32_t inputpos;
+
 #define BUFFERSIZE 4096
 static uint8_t buffer[BUFFERSIZE];
 
 static int buffered = 0;
 
-bool fetching = false;
-bool core_fetch = false;
-bool core_put = false;
-
 int32_t bt_data(uint8_t *data, int32_t len, uint32_t* sr) {
-    if (!core_fetch) {
-      core_fetch = true;
-      Serial.print("FETCHING CORE ");
-      Serial.println(xPortGetCoreID());
-    }
-    fetching = true;
-    int cnt = len;
     *sr = 44100;
-    if (cnt > buffered) {
-       Serial.println(buffered);
-       cnt = buffered;
+    if (len < 0 || data == NULL) {
+        bt_info("ERROR");
+        return 0;
     }
-    memmove(data, buffer, cnt);
-    buffered -= cnt;
-    memmove(buffer, buffer + cnt, buffered);
-    fetching = false;
+    int cnt = 0;
+    if (inputfile) {
+        cnt = inputfile.read(data, len);
+        if (cnt < len) {
+           bt_info("FINISH");
+           inputfile.close();
+        }
+    }
+    if (cnt < len) {
+        memset(data+cnt, 0, len - cnt);
+    }
     return cnt;
 }
 
 
 class AudioOutputBT : public AudioOutput {
-    void initDevice(const char *deviceName) {
-        a2dp_source_init((const char *)deviceName, "");
-        Serial.println("BlueTooth started");
-    }
+    bool started;
 public:
     AudioOutputBT(const char * device) {
-        initDevice(device);
+        a2dp_source_init(device, "");
+    }
+    void ConsumeFile(File wavfile, uint32_t pos) {
+        inputfile = wavfile;
+        inputpos = pos;
     }
     uint16_t ConsumeSamples(int16_t* samples, uint16_t count) override {
         Serial.println(count);
@@ -48,15 +48,11 @@ public:
     }
 
     bool ConsumeSample(int16_t samples[2]) {
-        
-        if (buffered >= BUFFERSIZE - 2 || fetching) {
-            if (!core_put) {
-              core_put = true;
-              Serial.print("PUTTING CORE ");
-              Serial.print(xPortGetCoreID());
-              Serial.print(" buffered ");
-              Serial.println(buffered);
-            }
+        if (!started) {
+            started = true;
+            Serial.println("Buffering start");
+        }
+        if (buffered >= BUFFERSIZE) {
             return false;
         }
         buffer[buffered++] = (samples[0] + 32768) / 257;
@@ -64,31 +60,35 @@ public:
         return true;
     }
     bool begin() override {
+        Serial.println("Playing start");
         hertz = 44100;
         return true;
     }
 
+    bool isIdle() {
+        return buffered == 0;
+    }
     void flush() override {
         buffered = 0;
     }
 
     bool stop() override {
-        flush();
+        Serial.println("Stop");
         return true;
     }
 
     bool SetRate(int hz) override {
         if (hertz != hz) {
-            hertz = hz;
-            flush();
+            Serial.println("samplerate mismatch");
         }
         return true;
     }
 
     bool SetChannels(int ch) override {
         if (channels != ch) {
+            Serial.print("channels set to ");
+            Serial.print(ch);
             channels = ch;
-            flush();
         }
         return true;
     }
--- MusicPlayer.ino
+++ MusicPlayer.ino
@@ -1,24 +1,29 @@
-#define TOUCH_CS // This sketch does not use touch, but this is defined to quiet the warning about not defining touch_cs.
-
-/*-------- CYD (Cheap Yellow Display) ----------*/
-#include <TFT_eSPI.h> // Hardware-specific library
-#include <SPI.h>
-#include "microSD.h"
-#include "playMp3.h"
-#include "storage.h"
 #include <WiFiManager.h>
+#include "microSD.h"
+#include "storage.h"
+#include "AudioOutputBT.h"
 #include "a2dp_source.h"
 
+// #define CYD -- when usign a CYD
+// #define RESET
+
+String playLists[20];
+String currentSong = "";
+int currentLine = 1;
+int maxLine = -1;
+
+#ifdef CYD
+/*-------- CYD (Cheap Yellow Display) ----------*/
+
+#define TOUCH_CS // This sketch does not use touch, but this is defined to quiet the warning about not defining touch_cs.
+
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
 TFT_eSPI tft = TFT_eSPI();              // Invoke custom library
 
-#define CYCLE_MS 10000
 #define SCREEN_HEIGHT 320
 #define SCREEN_WIDTH 240
 #define SPACER 20
-
-void bt_info(const char* info){
-    Serial.printf("bt_info: %s\n", info);
-}
 
 int screenCenterX = SCREEN_WIDTH / 2;
 uint8_t myFont = 1;
@@ -27,7 +32,7 @@ int myDatum = TL_DATUM;
 uint16_t myBackgroundColor = TFT_BLACK;
 uint16_t myFontColor = TFT_WHITE;
 
-void SetupCYD()
+void setupDisplay()
 {
   tft.init();
   tft.fillScreen(myBackgroundColor);
@@ -38,12 +43,85 @@ void SetupCYD()
   tft.setTextDatum(myDatum);
   tft.setTextPadding(SCREEN_WIDTH);
 }
+void coloredText(String msg, uint16_t color, int y, int font) {
+  tft.setTextColor(color, myBackgroundColor);
+  tft.drawCentreString(msg, screenCenterX, y, font);
+  tft.setTextColor(myFontColor, myBackgroundColor);
+}
+void showPlayLists() {
+  tft.fillScreen(myBackgroundColor);
+  coloredText("PlayLists", TFT_BLUE, 5, 2);
 
+  for (int line = 1; line <= maxLine; line++) {
+    Serial.println(playLists[line]);
+    if (line == currentLine) {
+      coloredText(playLists[line], TFT_YELLOW, 30 + SPACER * line, myFont * 2);
+    } else {
+      tft.drawCentreString(playLists[line], screenCenterX, 30 + SPACER * line, myFont);
+    }
+  }
+   coloredText(currentSong, TFT_YELLOW, SCREEN_HEIGHT - SPACER, myFont);
+}
+void displaySetup() {
+    tft.drawString("wifi: 'MusicPlayer'", 10, tft.height() - 100);
+    tft.drawString(WiFi.localIP().toString().c_str(), 10, tft.height() - 100 + SPACER);
+}
+#else
+// -- 0.96 inch I2S oled dislay
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define I2C_DEV_ADDR 0x3C
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+uint16_t myBackgroundColor = SSD1306_BLACK;
+uint16_t myFontColor = SSD1306_WHITE;
+
+void setupDisplay()
+{
+  display.begin(SSD1306_SWITCHCAPVCC, I2C_DEV_ADDR);
+  display.clearDisplay();
+  display.setTextColor(myFontColor);
+  display.setTextSize(1);
+  display.setCursor(4, 28);
+  display.println( "Welcome");
+  display.display();
+}
+void showPlayLists() {
+  display.clearDisplay();
+  display.setTextColor(myFontColor);
+  display.setTextSize(1);
+  display.setCursor(4, 28);
+  display.println(playLists[currentLine]);
+  display.println(currentSong);
+  display.display();
+}
+void displaySetup() {
+  display.clearDisplay();
+  display.setTextColor(myFontColor);
+  display.setTextSize(1);
+  display.setCursor(4, 28);
+  display.println("wifi: 'MusicPlayer'");
+  display.println(WiFi.localIP().toString().c_str());
+  display.display();
+}
+#endif
+
+uint32_t parseWAV(File audiofile);
+
+AudioOutputBT *out;
+
+void bt_info(const char* info){
+    Serial.printf("bt_info: %s\n", info);
+}
+void wav_info(const char* info){
+    Serial.printf("wav_info: %s", info);
+}
 nvs_handle_t nvs_storage;
 String bluetoothDevice = "";
-String currentSong = "";
-int currentLine = 1;
-int maxLine = -1;
 
 void showStates() {
   Serial.print("playList nr ");
@@ -56,21 +134,21 @@ void showStates() {
 
 void setupStates() {
   nvs_storage = initNvs();
+#ifndef RESET
   bluetoothDevice = getNonVolatile(nvs_storage, "device");
   currentSong = getNonVolatile(nvs_storage, "song");
   String numStr = getNonVolatile(nvs_storage, "line");
   if (!numStr.isEmpty()) {
     currentLine = atoi(numStr.c_str());
   }
+#endif
   if (bluetoothDevice.isEmpty()) {
     WiFiManager wm;
     WiFi.mode(WIFI_STA);
     wm.resetSettings();
     WiFiManagerParameter btDev = WiFiManagerParameter("device", "Bluetooth speaker", bluetoothDevice.c_str(), 50);
     wm.addParameter(&btDev);
-    tft.drawString("wifi: 'MusicPlayer'", 10, tft.height() - 100);
-    tft.drawString(WiFi.localIP().toString().c_str(), 10, tft.height() - 100 + SPACER);
-    
+    displaySetup();
     if (wm.autoConnect("MusicPlayer") && strlen(btDev.getValue()) && strcmp(bluetoothDevice.c_str(), btDev.getValue())) {
       bluetoothDevice = String(btDev.getValue());
       setNonVolatile(nvs_storage, "device", btDev.getValue());
@@ -79,10 +157,8 @@ void setupStates() {
     WiFi.mode(WIFI_OFF);
   }
   showStates();
-  setOutput(bluetoothDevice.c_str());
+  out = new AudioOutputBT(bluetoothDevice.c_str());
 }
-
-String playLists[20];
 
 void split(String dirs) {
   int idx = dirs.indexOf(";");
@@ -114,8 +190,8 @@ String getFirstSong() {
     Serial.println(retVal);
     return retVal;
   }
-  Serial.println("FAILED");
-  return "NOT FOUND";
+  Serial.println("NOT FOUND");
+  return "";
 }
 
 String getNextSong(const char *preveousSong) {
@@ -135,30 +211,11 @@ String getNextSong(const char *preveousSong) {
    Serial.printf("%s no  next song\n", preveousSong);
    return "";
 }
-void coloredText(String msg, uint16_t color, int y, int font) {
-  tft.setTextColor(color, myBackgroundColor);
-  tft.drawCentreString(msg, screenCenterX, y, font);
-  tft.setTextColor(myFontColor, myBackgroundColor);
-}
-void showPlayLists() {
-  tft.fillScreen(myBackgroundColor);
-  coloredText("PlayLists", TFT_BLUE, 5, 2);
-
-  for (int line = 1; line <= maxLine; line++) {
-    Serial.println(playLists[line]);
-    if (line == currentLine) {
-      coloredText(playLists[line], TFT_YELLOW, 30 + SPACER * line, myFont * 2);
-    } else {
-      tft.drawCentreString(playLists[line], screenCenterX, 30 + SPACER * line, myFont);
-    }
-  }
-   coloredText(currentSong, TFT_YELLOW, SCREEN_HEIGHT - SPACER, myFont);
-}
 /*-------- SETUP & LOOP ----------*/
 void setup()
 {
   Serial.begin(115200);
-  SetupCYD();
+  setupDisplay();
   initMicroSD();
   setupStates();
   String dirs = listDir("");
@@ -169,49 +226,47 @@ void setup()
     currentSong = "";
   }
   Serial.println(dirs);
-  if (currentSong.isEmpty()) {
+  while (currentSong.isEmpty()) {
     currentSong = getFirstSong();
-  }
-  showPlayLists();
-  setOutput(bluetoothDevice.c_str());
-}
-MP3STATE mp3State = MP3_DONE;
-long reported = 0;
-void loop()
-{
-  bt_loop();
-  String playingSong = "/" + playLists[currentLine] + "/" + currentSong;
-  if (mp3State != MP3_NOT_RUNNING) {
-    mp3State = playMp3(playingSong.c_str());
-  }
-  if (mp3State == MP3_STARTED) {
-    Serial.print(playingSong);
-    Serial.println(" started");
-    if (strcmp(currentSong.c_str(), getNonVolatile(nvs_storage, "song").c_str())) {
-      Serial.print("saving song ");
-      Serial.println(currentSong.c_str());
-      setNonVolatile(nvs_storage, "song", currentSong.c_str());
-    }
-  }
-  if (mp3State == MP3_DONE) {
-    Serial.print(playingSong); Serial.println(" finished");
-    String nextSong = getNextSong(currentSong.c_str());
-    Serial.print("Next song is "); Serial.println(nextSong);
-    if (nextSong.isEmpty()) {
-      Serial.println("Playlist done");
-      currentLine++;
+    if (currentSong.isEmpty()) {
+      Serial.print("Cannot find firstSong in this playlist ");
+      Serial.println(playLists[currentLine]);
+      currentLine += 1;
       if (currentLine > maxLine) {
         currentLine = 1;
       }
-      currentSong = getFirstSong();
-      Serial.print("saving list nr ");Serial.print(currentLine);
-      Serial.print("saving song ");Serial.println(currentSong.c_str());
-      setNonVolatile(nvs_storage, "line", String(currentLine).c_str());
-      setNonVolatile(nvs_storage, "song", currentSong.c_str());
-      showPlayLists();
-    } else {
-      currentSong = nextSong;
     }
-    coloredText( currentSong, TFT_YELLOW, SCREEN_HEIGHT - SPACER, myFont);
+  }
+  showPlayLists();
+}
+File playingWav;
+bool started = false;
+
+void loop()
+{
+  bt_loop();
+  String playingSong = "/" + playLists[currentLine] + "/" + currentSong ;
+  if (!playingWav) {
+    if (!started) {
+      started = true;
+      Serial.printf("Starting %s\n", playingSong.c_str());
+      playingWav = SD.open(playingSong.c_str());
+      out->ConsumeFile(playingWav, parseWAV(playingWav));
+    } else {
+      Serial.printf("Finished %s\n", playingSong.c_str());
+      currentSong = getNextSong(currentSong.c_str());
+      if (currentSong.isEmpty()) {
+          currentLine += 1;
+          if (currentLine > maxLine) {
+            currentLine = 1;
+          }
+          currentSong = getFirstSong();
+      }
+      if (!currentSong.isEmpty()) {
+        setNonVolatile(nvs_storage, "song", currentSong.c_str());
+        started = false;
+      }
+      showPlayLists();
+    }
   }
 }
